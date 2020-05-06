83e773fc9c215add1ff4f47cc4e891c8f68c84f1
SiriJustas <justas.sirinskas@gmail.com>
2020-05-07 00:01:29 +0300

    ADD: Added script for fast video rebuild from frames.

---
+++ reVideo.py
@@ -0,0 +1,27 @@
+import cv2
+import glob
+import os
+
+from pathlib import Path
+import shutil
+
+from os import walk
+
+def  saveVideo(frameR):
+	print('Starting to merge frames into video \n')
+	img_array = []
+	for filename in sorted(glob.glob('output/*.png'), key=os.path.getmtime):
+	    img = cv2.imread(filename)
+	    print(filename)
+	    height, width, layers = img.shape
+	    size = (width,height)
+	    img_array.append(img)
+	 
+	out = cv2.VideoWriter("output/outputvideo.avi",cv2.VideoWriter_fourcc(*'XVID'), frameR, size)
+	 
+	for i in range(len(img_array)):
+	    out.write(img_array[i])
+	out.release()
+
+
+saveVideo(30)
